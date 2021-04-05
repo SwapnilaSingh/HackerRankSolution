13d587e68db9b27e3340240bec597f5898a9d1a9
Swapnila.Singh <Swapnila.Singh@IN2429314W1.ey.net>
2021-04-05 11:10:56 +0530

    First commit along with hacker rank first problem solution

---
+++ src/main/java/Problem1.java
@@ -0,0 +1,11 @@
+
+public class Problem1 {
+	//Welcome to Java
+
+	    public static void main(String[] args) {
+	    System.out.println("Hello, World.");
+	    System.out.println("Hello, Java.");
+	    }
+	}
+
+
